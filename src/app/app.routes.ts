2b6fa497abd7dd23a5ff6ed1e5b1015790ca7a5b
Aleksej Mosunov <alexmosunov8@gmail.com>
2026-02-26 11:25:58 +0200

    amend some styles, add font family

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { PasswordGeneratorComponent } from './password-generator/password-generator';
+
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 
@@ -11,9 +11,5 @@ export const routes: Routes = [
   {
     path: 'about',
     component: About,
-  },
-  {
-    path: 'password-generator',
-    component: PasswordGeneratorComponent,
   },
 ];
