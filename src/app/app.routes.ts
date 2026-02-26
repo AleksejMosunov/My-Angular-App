121529cf415559ba0642ce8984824bc4b2e443c5
Aleksej Mosunov <alexmosunov8@gmail.com>
2026-02-26 17:46:12 +0200

    from SPA to SSR

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
   {
